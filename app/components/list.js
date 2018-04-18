ee3acb1c919f1bea098ec923cb3252e12a055d37
Andre Neves <andre@bighuman.com>
2018-04-17 22:32:44 -0400

    chore: fixes

--- app/components/list.js
+++ app/components/list.js
@@ -19,56 +19,105 @@ export default () => (
       </div>
     </li>
     <li className='list__item'>
-      <div className='list__item__image'>
-        <img src={btcIcon} alt='Bitcoin' />
+      <div className='list__item__info'>
+        <img
+          src={btcIcon}
+          alt='Bitcoin'
+          className='list__item__image'
+        />
+        <span className='list__item__name'>
+          Bitcoin
+        </span>
       </div>
       <div className='list__item__price'>
         $7,985.00
       </div>
     </li>
     <li className='list__item'>
-      <div className='list__item__image'>
-        <img src={btcIcon} alt='Bitcoin' />
+      <div className='list__item__info'>
+        <img
+          src={btcIcon}
+          alt='Bitcoin'
+          className='list__item__image'
+        />
+        <span className='list__item__name'>
+          Bitcoin
+        </span>
       </div>
       <div className='list__item__price'>
         $7,985.00
       </div>
     </li>
     <li className='list__item'>
-      <div className='list__item__image'>
-        <img src={btcIcon} alt='Bitcoin' />
+      <div className='list__item__info'>
+        <img
+          src={btcIcon}
+          alt='Bitcoin'
+          className='list__item__image'
+        />
+        <span className='list__item__name'>
+          Bitcoin
+        </span>
       </div>
       <div className='list__item__price'>
         $7,985.00
       </div>
     </li>
     <li className='list__item'>
-      <div className='list__item__image'>
-        <img src={btcIcon} alt='Bitcoin' />
+      <div className='list__item__info'>
+        <img
+          src={btcIcon}
+          alt='Bitcoin'
+          className='list__item__image'
+        />
+        <span className='list__item__name'>
+          Bitcoin
+        </span>
       </div>
       <div className='list__item__price'>
         $7,985.00
       </div>
     </li>
     <li className='list__item'>
-      <div className='list__item__image'>
-        <img src={btcIcon} alt='Bitcoin' />
+      <div className='list__item__info'>
+        <img
+          src={btcIcon}
+          alt='Bitcoin'
+          className='list__item__image'
+        />
+        <span className='list__item__name'>
+          Bitcoin
+        </span>
       </div>
       <div className='list__item__price'>
         $7,985.00
       </div>
     </li>
     <li className='list__item'>
-      <div className='list__item__image'>
-        <img src={btcIcon} alt='Bitcoin' />
+      <div className='list__item__info'>
+        <img
+          src={btcIcon}
+          alt='Bitcoin'
+          className='list__item__image'
+        />
+        <span className='list__item__name'>
+          Bitcoin
+        </span>
       </div>
       <div className='list__item__price'>
         $7,985.00
       </div>
     </li>
     <li className='list__item'>
-      <div className='list__item__image'>
-        <img src={btcIcon} alt='Bitcoin' />
+      <div className='list__item__info'>
+        <img
+          src={btcIcon}
+          alt='Bitcoin'
+          className='list__item__image'
+        />
+        <span className='list__item__name'>
+          Bitcoin
+        </span>
       </div>
       <div className='list__item__price'>
         $7,985.00
