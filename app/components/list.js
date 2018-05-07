64391bf859fe46d7de0a8668b268eac5d670cf17
André Neves <me@andreneves.me>
2018-05-06 23:08:43 -0400

    refactor: continued work on percentages

--- app/components/list.js
+++ app/components/list.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import numeral from 'numeral';
+import cx from 'classnames';
 import { getAssetImage } from '../utils/images';
 import Spinner from './spinner';
 
@@ -12,6 +13,25 @@ type Props = {
 }
 
 const getCurrentPrice = (prices, currency) => prices[currency];
+const getCurrentDelta = (prices, asset) => {
+  const openPrice = asset.dayOpen || 0;
+  const currentPrice = prices[asset.currency] || 0;
+  const delta = (currentPrice / openPrice) - 1;
+  const isNegative = delta < 0;
+  const sign = isNegative ? '-' : '+';
+  const value = isNegative ? delta.toString().split('-')[1] : delta;
+  const classnames = cx({
+    list__item__delta: true,
+    'list__item__delta--down': delta < 0,
+    'list__item__delta--up': delta > 0,
+  });
+
+  return (
+    <div className={classnames}>
+      {sign}{numeral(Number(value)).format('0.00%')}
+    </div>
+  );
+};
 
 const getListItems = (asset, prices) => (
   <li
@@ -28,6 +48,7 @@ const getListItems = (asset, prices) => (
         {asset.currency}
       </span>
     </div>
+    {getCurrentDelta(prices, asset)}
     <div className='list__item__price'>
       <div>
         {numeral(asset.marketCap).format('$ 0.00 a')}
